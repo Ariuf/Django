f117146a8e3e86827e1591cd5609c36f87245079
Aria <ariabaharmast@gmail.com>
2023-09-06 20:40:24 +0430

    new and final changes

--- cs50x/tasks/views.py
+++ cs50x/tasks/views.py
@@ -1,12 +1,34 @@
 from django.shortcuts import render
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+from django import forms
 
 # Create your views here.
-tasks = ['Do my homeworks', 'practice Python', 'check my emails']
+
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label = "NewTask")
 
 def index(request):
+    if "tasks" not in request.session:
+        request.session['tasks'] = []
+
     return render(request, 'tasks/index.html',{
-        "mytasks": tasks
+        "tasks": request.session['tasks']
     })
 
 def add(request):
-    return render(request, 'tasks/add.html')
+    if request.method == 'POST':
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data['task']
+            request.session['tasks'] += [task] 
+            return HttpResponseRedirect(reverse('tasks:index'))
+
+        else:
+            render(request, 'tasks/add.html', {
+        "form": form
+    })
+
+    return render(request, 'tasks/add.html', {
+        "form": NewTaskForm()
+    })
